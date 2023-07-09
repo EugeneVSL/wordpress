588657f0bb665d5f9f96bb802c3289e73a3d07f4
Eugene <eugenevasile@gmail.com>
2023-07-09 19:15:48 +0200

    Added page template and fixed some links. Added dynamic page title
    Added page template and fixed some links. Added dynamic page title

--- wp-content/themes/devtask/functions.php
+++ wp-content/themes/devtask/functions.php
@@ -34,7 +34,7 @@ if ( ! function_exists( 'devtask_scripts_styles' ) ) :
 		wp_enqueue_script("devtask-bootstrap-script", "//cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js");
 
 		// Enqueue app script.
-		wp_enqueue_script('devtask-script', get_theme_file_uri('/src/index.js'), NULL, true);
+		wp_enqueue_script('devtask-script', get_theme_file_uri('/src/index.js'), [], true);
 
 		// Enqueue theme stylesheet.
 		wp_enqueue_style("devtask-style", get_template_directory_uri() . "/style.css");
@@ -43,3 +43,15 @@ if ( ! function_exists( 'devtask_scripts_styles' ) ) :
 endif;
 
 add_action( 'wp_enqueue_scripts', 'devtask_scripts_styles' );
+
+if ( ! function_exists( 'website_features' ) ) :
+
+	function website_features() {
+
+		// add title to the current page
+		add_theme_support('title-tag');
+	}
+
+endif;
+
+add_action('after_setup_theme', 'website_features');
